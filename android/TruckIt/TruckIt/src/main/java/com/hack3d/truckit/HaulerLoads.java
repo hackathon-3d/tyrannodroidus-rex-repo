a273d19623529480a5a8664dc3e0c981cf44721d
johngyselinck <johngyselinck@gmail.com>
2013-08-24 02:32:47 -0400

    Some more updates

--- android/TruckIt/TruckIt/src/main/java/com/hack3d/truckit/HaulerLoads.java
+++ android/TruckIt/TruckIt/src/main/java/com/hack3d/truckit/HaulerLoads.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 /**
  * Created by johngyselinck on 8/24/13.
  */
-public class HaulerProfile extends Activity {
+public class HaulerLoads extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
-        setContentView(R.layout.hauler_profile);
+        setContentView(R.layout.hauler_loads);
     }
 }
