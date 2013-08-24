2ccad329c72449c2cc2b0fcf515d52224bca8190
johngyselinck <johngyselinck@gmail.com>
2013-08-24 01:35:31 -0400

    Hauler stuff almost all layed out

--- android/TruckIt/TruckIt/src/main/java/com/hack3d/truckit/HaulerHome.java
+++ android/TruckIt/TruckIt/src/main/java/com/hack3d/truckit/HaulerHome.java
@@ -1,7 +1,9 @@
 package com.hack3d.truckit;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by johngyselinck on 8/24/13.
@@ -12,5 +14,11 @@ public class HaulerHome extends Activity {
     protected void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
         setContentView(R.layout.hauler_main);
+    }
+
+    //When clicking on the Haul a Load button
+    public void haulerProfile(View view){
+        Intent intent = new Intent(this, HaulerProfile.class);
+        startActivity(intent);
     }
 }
